3a448e6648fec4f75fa5f548306922c5ed5c297f
2025050067 <khk013000@gmail.com>
2026-05-27 15:39:10 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-     char arr[100]; 
-     char *p; 
-     char *q; 
-     char maxChar;
+     char arr[10]; 
+     char *p=nullptr; 
+     char *q=nullptr; 
+     char maxChar='\0';
      int maxCount=0; 
      int count=0;
     for(p=arr;p<arr+10;p++){
